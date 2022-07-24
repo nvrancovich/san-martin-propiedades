6fea20ba0181253de939e8063d05f98e5a6c4a0e
nvrancovich <nvrancovich@gmail.com>
2022-07-24 18:21:37 -0300

    Cambios generales en el main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 df = pd.read_csv('features_propiedades_final.csv', index_col=0)
 segments_avg = pd.read_csv('promedios_segmentos.csv', index_col=0)
 
-st.header("Tasador de propiedades dentro del partido de General San Martín")
+st.header("Cuanto Vale tu Propiedad: General San Martín")
 st.text_input("Ingresá tu nombre: ", key="name")
 
 if st.checkbox('Mostrar muestra del set de datos de entrenamiento'):
@@ -28,8 +28,8 @@ with left_column:
         'Segmento donde se encuentra la propiedad:',
         np.unique(segments_avg['area']))
 
-feature_covered_surface_m2 = st.slider('Superficie cubierta en m2', 0.0, max(df['covered_surface_m2']), 1.0)  
-feature_bedrooms = st.slider('Cantidad de dormitorios', min_value=min(df['bedrooms']), max_value=max(df['bedrooms']))  
-feature_bathrooms = st.slider('Cantidad de baños', min_value=min(df['bathrooms']), max_value=max(df['bathrooms']))  
+feature_covered_surface_m2 = st.slider('Superficie cubierta en m2', min_value=min(df['covered_surface_m2']), max_value=max(df['covered_surface_m2']), step = 1)  
+feature_bedrooms = st.slider('Cantidad de dormitorios', min_value=min(df['bedrooms']), max_value=max(df['bedrooms']), step = 1)  
+feature_bathrooms = st.slider('Cantidad de baños', min_value=min(df['bathrooms']), max_value=max(df['bathrooms']), step = 1)  
 feature_latitude = st.slider('Latitud en el mapa (aproximada)', min_value=min(df['latitude']), max_value=max(df['latitude']),step=0.0001)  
 feature_longitude = st.slider('Longitud en el mapa (aproximado)', min_value=min(df['longitude']), max_value=max(df['longitude']),step=0.0001)  

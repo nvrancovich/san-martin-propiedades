ce0924fdec97d52e19aaeb6d0beb39feddc69bfa
nvrancovich <nvrancovich@gmail.com>
2022-07-26 10:35:39 -0300

    Arreglado error de columnas

--- main.py
+++ main.py
@@ -34,16 +34,14 @@ with left_column:
         'Segmento donde se encuentra la propiedad:',
         np.unique(segments_avg.index))
 
-left_column, right_column = st.columns(2)
-with right_column:
-    feature_covered_surface_m2 = st.slider('Superficie cubierta en m2:', min_value=min(df['covered_surface_m2']), max_value=max(df['covered_surface_m2']), step = 1.0)  
-    feature_bedrooms = st.slider('Cantidad de dormitorios:', min_value=min(df['bedrooms']), max_value=max(df['bedrooms']), step = 1.0)  
-    feature_bathrooms = st.slider('Cantidad de baños:', min_value=min(df['bathrooms']), max_value=max(df['bathrooms']), step = 1.0)  
+feature_covered_surface_m2 = st.slider('Superficie cubierta en m2:', min_value=min(df['covered_surface_m2']), max_value=max(df['covered_surface_m2']), step = 1.0)  
+feature_bedrooms = st.slider('Cantidad de dormitorios:', min_value=min(df['bedrooms']), max_value=max(df['bedrooms']), step = 1.0)  
+feature_bathrooms = st.slider('Cantidad de baños:', min_value=min(df['bathrooms']), max_value=max(df['bathrooms']), step = 1.0)  
 
-    st.write('Si no sabés como averiguar las coordenadas geográficas, te dejo un ejemplo de 10 segundos [acá](https://youtube.com/shorts/iiJCZwrW3ps?feature=share).')
+st.write('Si no sabés como averiguar las coordenadas geográficas, te dejo un ejemplo de 10 segundos [acá](https://youtube.com/shorts/iiJCZwrW3ps?feature=share).')
 
-    feature_latitude = st.slider('Latitud en el mapa (aproximada):', min_value=min(df['latitude']), max_value=max(df['latitude']),step=0.0001)  
-    feature_longitude = st.slider('Longitud en el mapa (aproximado):', min_value=min(df['longitude']), max_value=max(df['longitude']),step=0.0001)  
+feature_latitude = st.slider('Latitud en el mapa (aproximada):', min_value=min(df['latitude']), max_value=max(df['latitude']),step=0.01)  
+feature_longitude = st.slider('Longitud en el mapa (aproximado):', min_value=min(df['longitude']), max_value=max(df['longitude']),step=0.01)  
 
 if st.button('Hacer predicción'):
 

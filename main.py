d7bca1c90b096b39a2efa7b06d849fb544504714
nvrancovich <nvrancovich@gmail.com>
2022-07-26 10:43:19 -0300

    Agregando limpieza del dataset en el main

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ df = pd.read_csv('propiedades.csv', index_col=0)
 propiety_tipes = {'Casa':'casa', 'Departamento':'departamento', 'Propiedad Horizontal':'ph'}
 segments_avg = pd.read_csv('promedios_segmentos.csv', index_col=0,usecols=['area','price/m2'])
 
+df['bathrooms'] = df['bathrooms'].fillna(1)
+df.dropna(subset=['bedrooms'], axis=0, inplace=True)
+df.dropna(subset=['latitude'], axis=0, inplace=True)
+df.dropna(subset=['covered_surface_m2'], axis=0, inplace=True)
+df.dropna(subset=['price_USD'], axis=0, inplace=True)
+df = df[df.bedrooms < 10]
+df = df[['price_USD','type','covered_surface_m2','bedrooms','price/m2','bathrooms','latitude','longitude']]
+df.reset_index(drop=True, inplace=True)
+
 st.header("Cuanto Vale tu Propiedad: General San Martín")
 st.text_input("Ingresá tu nombre: ", key="name")
 

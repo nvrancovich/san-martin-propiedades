1eaa67edc939cc866fbf22d8ccc00e1842f1c86a
nvrancovich <nvrancovich@gmail.com>
2022-07-26 09:40:04 -0300

    Cambiando el tipo de input para las coordenadas

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import xgboost as xgb
 xgboost_regressor = xgb.XGBRegressor()
 xgboost_regressor.load_model("xgboost_regressor.json")
 
-df = pd.read_csv('features_propiedades_final.csv', index_col=0)
+df = pd.read_csv('propiedades.csv', index_col=0)
 propiety_tipes = {'Casa':'casa', 'Departamento':'departamento', 'Propiedad Horizontal':'ph'}
 segments_avg = pd.read_csv('promedios_segmentos.csv', index_col=0,usecols=['area','price/m2'])
 
@@ -16,7 +16,7 @@ st.text_input("Ingresá tu nombre: ", key="name")
 if st.checkbox('Mostrar muestra del set de datos de entrenamiento'):
     df[:30]
 
-st.subheader("Por favor ingresar las caracteristicas de la propiedad:")
+st.subheader("Ingresar características de la propiedad:")
 
 left_column, right_column = st.columns(2)
 with left_column:
@@ -25,7 +25,7 @@ with left_column:
         list(propiety_tipes.keys()))
 
 
-st.write('Los diferentes segmentos pueden consultarse en el mapa haciendo [acá](https://www.google.com/maps/d/u/0/edit?mid=1D53sXpkQJc8f3ESd5F4SNj92LI0rsRk&usp=sharing)')
+st.write('Los diferentes segmentos pueden consultarse en el mapa haciendo [acá](https://www.google.com/maps/d/u/0/edit?mid=1D53sXpkQJc8f3ESd5F4SNj92LI0rsRk&usp=sharing) (tener abierto Google Maps previamente si no funciona).')
 
 left_column, right_column = st.columns(2)
 
@@ -37,8 +37,8 @@ with left_column:
 feature_covered_surface_m2 = st.slider('Superficie cubierta en m2', min_value=min(df['covered_surface_m2']), max_value=max(df['covered_surface_m2']), step = 1.0)  
 feature_bedrooms = st.slider('Cantidad de dormitorios', min_value=min(df['bedrooms']), max_value=max(df['bedrooms']), step = 1.0)  
 feature_bathrooms = st.slider('Cantidad de baños', min_value=min(df['bathrooms']), max_value=max(df['bathrooms']), step = 1.0)  
-feature_latitude = st.slider('Latitud en el mapa (aproximada)', min_value=min(df['latitude']), max_value=max(df['latitude']),step=0.0001)  
-feature_longitude = st.slider('Longitud en el mapa (aproximado)', min_value=min(df['longitude']), max_value=max(df['longitude']),step=0.0001)  
+feature_latitude = st.number_input('Latitud en el mapa (aproximada)', min_value=min(df['latitude']), max_value=max(df['latitude']),step=0.0001)  
+feature_longitude = st.number_input('Longitud en el mapa (aproximado)', min_value=min(df['longitude']), max_value=max(df['longitude']),step=0.0001)  
 
 if st.button('Hacer predicción'):
 

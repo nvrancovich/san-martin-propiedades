1c28297f29aa4f1b248cf44b66fa2f0d19bc8330
nvrancovich <nvrancovich@gmail.com>
2022-07-26 10:31:59 -0300

    Cambios en los slides y textos

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ segments_avg = pd.read_csv('promedios_segmentos.csv', index_col=0,usecols=['area
 st.header("Cuanto Vale tu Propiedad: General San Martín")
 st.text_input("Ingresá tu nombre: ", key="name")
 
-if st.checkbox('Mostrar muestra del set de datos de entrenamiento'):
+if st.checkbox('Ver muestra del set de datos de entrenamiento'):
     df[:30]
 
 st.subheader("Ingresar características de la propiedad:")
@@ -25,7 +25,7 @@ with left_column:
         list(propiety_tipes.keys()))
 
 
-st.write('Los diferentes segmentos pueden consultarse en el mapa haciendo [acá](https://www.google.com/maps/d/u/0/edit?mid=1D53sXpkQJc8f3ESd5F4SNj92LI0rsRk&usp=sharing) (tener abierto Google Maps previamente si no funciona).')
+st.write('Los diferentes segmentos pueden consultarse en el mapa tocando [acá](https://www.google.com/maps/d/u/0/edit?mid=1D53sXpkQJc8f3ESd5F4SNj92LI0rsRk&usp=sharing) (tener abierto Google Maps previamente).')
 
 left_column, right_column = st.columns(2)
 
@@ -34,11 +34,16 @@ with left_column:
         'Segmento donde se encuentra la propiedad:',
         np.unique(segments_avg.index))
 
-feature_covered_surface_m2 = st.slider('Superficie cubierta en m2', min_value=min(df['covered_surface_m2']), max_value=max(df['covered_surface_m2']), step = 1.0)  
-feature_bedrooms = st.slider('Cantidad de dormitorios', min_value=min(df['bedrooms']), max_value=max(df['bedrooms']), step = 1.0)  
-feature_bathrooms = st.slider('Cantidad de baños', min_value=min(df['bathrooms']), max_value=max(df['bathrooms']), step = 1.0)  
-feature_latitude = st.number_input('Latitud en el mapa (aproximada)', min_value=min(df['latitude']), max_value=max(df['latitude']),step=0.0001)  
-feature_longitude = st.number_input('Longitud en el mapa (aproximado)', min_value=min(df['longitude']), max_value=max(df['longitude']),step=0.0001)  
+left_column, right_column = st.columns(2)
+with right_column:
+    feature_covered_surface_m2 = st.slider('Superficie cubierta en m2:', min_value=min(df['covered_surface_m2']), max_value=max(df['covered_surface_m2']), step = 1.0)  
+    feature_bedrooms = st.slider('Cantidad de dormitorios:', min_value=min(df['bedrooms']), max_value=max(df['bedrooms']), step = 1.0)  
+    feature_bathrooms = st.slider('Cantidad de baños:', min_value=min(df['bathrooms']), max_value=max(df['bathrooms']), step = 1.0)  
+
+    st.write('Si no sabés como averiguar las coordenadas geográficas, te dejo un ejemplo de 10 segundos [acá](https://youtube.com/shorts/iiJCZwrW3ps?feature=share).')
+
+    feature_latitude = st.slider('Latitud en el mapa (aproximada):', min_value=min(df['latitude']), max_value=max(df['latitude']),step=0.0001)  
+    feature_longitude = st.slider('Longitud en el mapa (aproximado):', min_value=min(df['longitude']), max_value=max(df['longitude']),step=0.0001)  
 
 if st.button('Hacer predicción'):
 
